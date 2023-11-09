f55f11c1cd4dd403aaf4089839cdf619aca16ef0
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2023-11-09 12:22:56 +0330

    Update main.py

--- Random answer generation/main.py
+++ Random answer generation/main.py
@@ -1,1 +1,9 @@
+import numpy as np
 
+week_days = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+probabilities = [0.195, 0.19, 0.19, 0.19, 0.19, 0.04, 0.005]
+
+arr = np.random.choice(week_days, size=500, p=probabilities)
+for wd in week_days:
+    rep = len(arr[arr == wd])
+    print("number of {} is : {}".format(wd, rep))
